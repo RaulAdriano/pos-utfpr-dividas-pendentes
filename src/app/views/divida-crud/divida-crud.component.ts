38e642314411df76eead15f18666588a0e0bb53f
RaulAdriano <raul.adriano.ramos@gmail.com>
2022-10-09 23:36:12 -0300

    navegação entre telas

---
+++ src/app/views/divida-crud/divida-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-divida-crud',
+  templateUrl: './divida-crud.component.html',
+  styleUrls: ['./divida-crud.component.scss']
+})
+export class DividaCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
